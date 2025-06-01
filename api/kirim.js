79f07885c782eadfe6ee37685f3a88250a195b69
Krisna Aditya Pratama <krisnaadityapratama@students.amikom.ac.id>
2025-06-01 16:04:51 +0700

    Update

--- api/kirim.js
+++ api/kirim.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Metode hanya POST" });
+    return res.status(405).json({ error: "Metodee hanya POST" });
   }
 
   try {
